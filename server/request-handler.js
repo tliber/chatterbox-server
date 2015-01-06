dfb3f4e95936bc1cfe5ff86904dd334f298e5a6b
Hack Reactor Students <accounts@hackreactor.com>
2015-01-05 20:12:42 -0800

    10/12 tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,7 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
+module.exports.requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -29,8 +29,39 @@ var requestHandler = function(request, response) {
   // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
 
+  var result = [];
+  var messageData = {'results':result};
+  //messageData = JSON.stringify(messageData);
   // The outgoing status.
-  var statusCode = 200;
+  if (request.method === 'GET'){
+    var statusCode = 200;
+    messageData = JSON.stringify(messageData);
+    response.end(messageData);
+  };
+
+  if (request.method === 'POST'){
+    var statusCode = 201;
+    var testString = '';
+
+    //var newMessage = {};
+
+
+    // newMessage.username = request.username || '';
+    // newMessage.message = request.message || '';
+    // newMessage.room = request.room || '';
+    // newMessage.createdAt = new Date();
+
+    request.on('data',function(chunk){
+      testString = testString.concat(chunk);
+    });
+    request.on('end',function(){
+      var newMessage = JSON.parse(testString);
+      result.push(newMessage);
+      console.log(messageData);
+      messageData = JSON.stringify(messageData);
+      response.end(messageData);
+    });
+  };
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
@@ -39,7 +70,7 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/json";
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -49,10 +80,12 @@ var requestHandler = function(request, response) {
   // anything back to the client until you do. The string you pass to
   // response.end() will be the body of the response - i.e. what shows
   // up in the browser.
-  //
+
+  //var messageData = {'results':results};
+  //messageData = JSON.stringify(messageData);
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+  //response.end(messageData);
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
